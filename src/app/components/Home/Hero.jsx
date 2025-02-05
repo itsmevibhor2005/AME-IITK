326afcbf12a2b636fb06c561949b8f1c3492dc81
itsmevibhor2005 <vibhorsri2005@gmail.com>
2025-02-06 02:21:07 +0530

    hero

--- src/app/components/Home/Hero.jsx
+++ src/app/components/Home/Hero.jsx
@@ -8,6 +8,9 @@ import Spline from '@splinetool/react-spline';
 import React from 'react'
 import FuturisticRobotLoader from './Robot';
 import dynamic from "next/dynamic";
+import { motion } from "framer-motion";
+
+// import Home from "../../../../home.jpg"
 
 
 
@@ -16,22 +19,27 @@ const Hero = () => {
       // ssr: false,
     // });
   return (
-    <div className="flex items-center justify-between min-h-[90vh] p-6 mx-10">
-      <div className="flex-1 mr-6 w-[45vw] absolute bottom-[30%]">
+    <div className="flex items-center justify-between min-h-[85vh] p-2 mx-5">
+      <div className="flex-1 mr-6 w-[45vw] bottom-[30%]">
         {/* <h2 className="text-6xl font-bold mb-[40px] text-center">ABOUT US</h2> */}
         {/* <GradualSpacing
           text="ASSOCIATION OF MECHANICAL ENGINEERS, IIT KANPUR"
           className="text-6xl font-bold mb-[40px] text-center"
         /> */}
 
-        <BlurIn
-          className="text-gray-700 text-6xl tracking-wide"
-          word="ASSOCIATION OF MECHANICAL ENGINEERS, IIT KANPUR"
-        />
+        <motion.h1
+        className="text-5xl leading-tight font-bold text-gray-700 mb-10 text-center"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
+        ASSOCIATION OF MECHANICAL ENGINEERS, IIT KANPUR
+      </motion.h1>
+
       </div>
-      <div className="flex-none w-[70%] h-[90vh] absolute top-14 right-0 z-10">
+      <div className="flex-none w-[50%] rounded-md">
         {" "}
-        */
+        <img src="/home.jpg" alt="" className='rounded-md'/>
         {/* <Spline scene="https://prod.spline.design/qsqKwIv3haEfn6Pa/scene.splinecode" /> */}
         {/* <FuturisticRobotLoader /> */}
       </div>
